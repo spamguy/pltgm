b3b39800cfb070b1fbdbd4f4349f4cc92130553f
spamguy <spamguy@spamguy.org>
2025-10-13 12:47:57 -0700

    Fix formatting config.

--- server/src/dictionary/index.ts
+++ server/src/dictionary/index.ts
@@ -1,11 +1,8 @@
-import { stat, createReadStream } from 'fs/promises';
-
 const DICTIONARY_PATH = '../../assets/dictionary.json';
 
 export async function verifyDictionary(): Promise<void> {
-	const fileStat = await stat(DICTIONARY_PATH);
-	if (!fileStat) {
-		fileStat.
+	const fileStat = await Deno.stat(DICTIONARY_PATH);
+	if (!fileStat.isFile) {
+		Deno.create(DICTIONARY_PATH);
 	}
-		const readStream = createReadStream(DICTIONARY_PATH);
 }
