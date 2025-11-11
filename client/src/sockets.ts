2fabc3ca55c4501885403b5d8ec4d20ffc10ec95
spamguy <spamguy@spamguy.org>
2025-11-10 17:11:07 -0800

    Updating things.

---
+++ client/src/sockets.ts
@@ -0,0 +1,17 @@
+import { reactive } from 'vue';
+import { io } from 'socket.io-client';
+
+export const state = reactive({
+	connected: false,
+	fooEvents: [],
+	barEvents: [],
+});
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+
+export const socket = io(URL);
+
+socket.on('connect', () => {
+	state.connected = true;
+});
